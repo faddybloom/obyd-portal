0b00902b5f6e6e95cde7adf9cead75ecfe8dc528
Mark Russell <mark.russell@electralink.co.uk>
2025-06-24 10:30:51 +0100

    fixing deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@aws-amplify/backend',
+        '@aws-amplify/backend-cli',
+        'aws-cdk',
+        'aws-cdk-lib',
+        'constructs'
+      ]
+    }
+  }
 })
