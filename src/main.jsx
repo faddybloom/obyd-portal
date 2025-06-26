2f642a96a7fb3fdd7ef9d942d8dd8952250e57a9
Mark Russell <mark.russell@electralink.co.uk>
2025-06-26 09:53:29 +0100

    trying to resolve browser errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './amplify-config'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+
+Amplify.configure(outputs)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
