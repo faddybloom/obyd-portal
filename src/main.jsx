3da9f9fc2204947d88db9e3bdf5f0449cfa1b777
Mark Russell <mark.russell@electralink.co.uk>
2025-06-26 10:30:10 +0100

    trying to resolve browser errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Amplify } from 'aws-amplify'
+import outputs from "../amplify_outputs.json";
 
-// Configure Amplify - outputs will be injected by Amplify Gen 2
-try {
-  const outputs = await import('../amplify_outputs.json')
-  Amplify.configure(outputs.default || outputs)
-} catch {
-  // Fallback configuration will be provided by Amplify Gen 2
-  console.log('Using Amplify Gen 2 auto-configuration')
-}
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
